9c71b32d25ad6a31b124840bc63f52c4f927dc4e
kzhao189 <kzzzhao@gmail.com>
2025-01-24 16:50:07 -0500

    tail added

--- JellyFish/Assets/tentacle.cs
+++ JellyFish/Assets/tentacle.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class tentacle : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int length;
+    [SerializeField] LineRenderer lineRend;
+    [SerializeField] Vector3[] segmentPoses;
+    private Vector3[] segmentV;
+
+    [SerializeField] Transform targetDir;
+    [SerializeField] float targetDist;
+    [SerializeField] float smoothSpeed;
+
     void Start()
     {
-        
+        lineRend.positionCount = length;
+        segmentPoses = new Vector3[length];
+        segmentV = new Vector3[length];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        segmentPoses[0] = targetDir.position;
+
+        for (int i = 1; i < segmentPoses.Length; i++)
+        {
+            segmentPoses[i] = Vector3.SmoothDamp(segmentPoses[i], segmentPoses[i - 1] + targetDir.right * targetDist, ref segmentV[i], smoothSpeed);
+        }
+        lineRend.SetPositions(segmentPoses);
     }
 }
