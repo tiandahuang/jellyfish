0af860617294c350707869fdfbbf90e9e65bb921
Katie <kzzzhao@gmail.com>
2025-01-25 11:33:32 -0500

    Environment setup (#6)
    * VR setting
    
    OVR Hands
    BLE setting
    
    * jelly added
    
    * tail added
    
    * prefabbed jelly, more tentacle
    
    * switched to URP
    
    * jelly is jellying
    
    * volume added background created
    
    * setdressing and added corals
    
    * packages fix
    
    ---------
    
    Co-authored-by: dldnwlsgd <eugene5759@snu.ac.kr>

---
+++ JellyFish/Assets/tentacle.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//code from here https://www.youtube.com/watch?v=9hTnlp9_wX8
+
+public class tentacle : MonoBehaviour
+{
+    [SerializeField] int length;
+    [SerializeField] LineRenderer lineRend;
+    [SerializeField] Vector3[] segmentPoses;
+    private Vector3[] segmentV;
+
+    [SerializeField] Transform targetDir;
+    [SerializeField] float targetDist;
+    [SerializeField] float smoothSpeed;
+    [SerializeField] float trailSpeed;
+
+    [SerializeField] float wiggleSpeed;
+    [SerializeField] float wiggleMagnitude;
+    [SerializeField] Transform wiggleDir;
+
+    void Start()
+    {
+        lineRend.positionCount = length;
+        segmentPoses = new Vector3[length];
+        segmentV = new Vector3[length];
+    }
+
+    void Update()
+    {
+
+        wiggleDir.localRotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * wiggleSpeed) * wiggleMagnitude);
+
+        segmentPoses[0] = targetDir.position;
+
+        for (int i = 1; i < segmentPoses.Length; i++)
+        {
+            segmentPoses[i] = Vector3.SmoothDamp(segmentPoses[i], segmentPoses[i - 1] + targetDir.right * targetDist, ref segmentV[i], smoothSpeed);
+        }
+        lineRend.SetPositions(segmentPoses);
+    }
+}
