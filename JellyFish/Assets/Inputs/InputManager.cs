0d44a04eed2d8f5673f862156e763fc650f4346c
Matthew L. Komar <46490210+MatthewLKomar@users.noreply.github.com>
2025-01-25 11:13:02 -0500

    Mk/inputs (#4)
    * added new input system with example inputs
    
    * IDE auto-gen folders added to gitignore
    
    * cleanliness is next to godliness

---
+++ JellyFish/Assets/Inputs/InputManager.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+public class InputManager : MonoBehaviour
+{
+    public InputActionAsset inputActionAsset;
+
+    private InputAction floatAction;
+    private InputActionMap inputActionMap;
+
+    void OnEnable()
+    {
+        if (inputActionAsset != null)
+        {
+            #if UNITY_EDITOR
+                inputActionMap = inputActionAsset.FindActionMap("PC");
+            #elif UNITY_ANDROID
+                // Sus does this actually work??
+                inputActionMap = inputActionAsset.FindActionMap("Quest");
+            #endif
+            
+            if (inputActionMap != null)
+            {
+                print("map not null");
+                floatAction = inputActionMap.FindAction("Float");
+            }
+
+            if (floatAction != null)
+            {
+                print("float not null");
+                floatAction.Enable();
+                floatAction.performed += OnFloatActionPerformed;
+            }
+            // TODO bind to BLE signal here
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (floatAction != null)
+        {
+            floatAction.Disable();
+            floatAction.performed -= OnFloatActionPerformed;
+        }
+        
+        // TODO un bind to BLE signal here
+    }
+    
+    private void OnFloatActionPerformed(InputAction.CallbackContext context)
+    {
+        // Get the float value
+        float value = context.ReadValue<float>();
+        Debug.Log($"Float value received: {value}");
+    }
+}
